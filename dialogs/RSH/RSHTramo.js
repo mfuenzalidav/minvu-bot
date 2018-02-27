552219d3dd16a1a64301f1e11eb0b0860956dbe2
Guillermo Parra Astorga <gparra@minvu.cl>
2018-02-27 18:33:59 -0300

    Merge rama de Camila y Guillermo.

--- dialogs/RSH/RSHTramo.js
+++ dialogs/RSH/RSHTramo.js
@@ -1,5 +1,5 @@
-const soap = require('soap')
-var Rut = require('rutjs')
+const soap  = require('soap')
+var Rut     = require('rutjs')
 
 
 function RSHTramo(builder) {
@@ -30,11 +30,11 @@ function RSHTramo(builder) {
 
         var rut = new Rut(results.response);
         var digitos = rut.rut;
-        var verificador = rut.checkDigit;
+        var verificador = rut.checkDigit;    
 
         var args = { entradaRSH: { Rut: digitos, Dv: verificador, Periodo: '-1', UsSist: '1' } };
 
-        session.send('Ha consultado el tramo del rut: ' + rut.getCleanRut());
+        session.send('Ha consultado el tramo del rut: ' + rut.getNiceRut());
 
         soap.createClient('http://wsminvuni.test.minvu.cl/WSICEMds/RegistroSocialHogares.svc?singleWsdl', function (err, client) {
             if (err) {
@@ -49,8 +49,10 @@ function RSHTramo(builder) {
                         session.send('Con respecto a su consulta al tramo en RSH, lo lamento, no pude obtener datos del servicio RSH')
                     }
                     else {
-                        if (result.ObtenerRegistroSocialHogaresResult.RESPUESTA.salidaRSH.Estado === 1)
+                        if (result.ObtenerRegistroSocialHogaresResult.RESPUESTA.salidaRSH.Estado === 1){
+                            const tramo = result.ObtenerRegistroSocialHogaresResult.RESPUESTA.salidaRSH.RshMinvu.Tramo
                             session.send('Con respecto a su consulta al tramo en RSH, el tramo del rut ' + rut.getNiceRut() + ' es ' + tramo);
+                        }
                         else if (result.ObtenerRegistroSocialHogaresResult.RESPUESTA.salidaRSH.Estado === 2)
                             session.send('Con respecto a su consulta al tramo en RSH, el rut ' + rut.getNiceRut() + ' no tiene registros en RSH');
                         else
